8203e666d11452099bf998bffd149a20c23cc647
SahinAlkan0 <sahinalkan134@gmail.com>
2025-01-13 09:43:35 +0300

    TersUcgen

--- src/TersUcgen.java
+++ src/TersUcgen.java
@@ -4,10 +4,10 @@ public class TersUcgen {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Basamak sayısı giriniz = ");
-        int sayı = inp.nextInt();
+        int n = inp.nextInt();
 
-        for (int i = sayı; i > 0; i--) {
-            for (int k = (sayı - i); k >= 0; k--) {
+        for (int i = n; i > 0; i--) {
+            for (int k = (n - i); k >= 0; k--) {
                 System.out.print(" ");
             }
             for (int l = (2 * i - 2); l >= 0; l--) {
